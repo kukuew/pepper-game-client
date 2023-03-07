df9827d9f5ebe1375b34a640e08af7e7cec405ee
Mykola Smuzhanytsia <smuzhanytsia@gmail.com>
2023-03-07 14:44:07 +0100

    style: reformat code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/400.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
