6d7eb1c5f04f82e34ba8088951886bb7c214ab93
Mykola Smuzhanytsia <smuzhanytsia@gmail.com>
2023-03-07 21:55:00 +0100

    feat: fetch data from API and display game components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material'
+import { grey } from '@mui/material/colors';
+import { useQuery } from '@apollo/client'
+import { LIST_STARSHIPS } from './graphql/queries'
+import { Starship } from './types'
+import Game from './components/Game'
+
 function App() {
-  return <div></div>
+  const { loading, data } = useQuery<{ listStarships: Starship[] }>(LIST_STARSHIPS)
+
+  return (
+    <Stack alignItems="center" bgcolor={grey[100]} minHeight="100vh">
+      <Typography variant="h3" component="h1" py={5}>
+        {loading ? 'Game is loading...' : "Pepper Game!"}
+      </Typography>
+      {data && <Game starships={data.listStarships} />}
+    </Stack>
+  )
 }
 
 export default App
